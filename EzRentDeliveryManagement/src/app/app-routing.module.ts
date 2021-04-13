a8022ed639721c73a386bd5451894868c3f984c4
lxin42140 <lxin42140@gmail.com>
2021-04-13 13:53:04 +0800

    added profile

--- EzRentDeliveryManagement/src/app/app-routing.module.ts
+++ EzRentDeliveryManagement/src/app/app-routing.module.ts
@@ -1,17 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {ViewAllTransactionsComponent} from './view-all-transactions/view-all-transactions.component';
+import { ViewAllTransactionsComponent } from './view-all-transactions/view-all-transactions.component';
 
 import { ViewAllOngoingDeliveriesComponent } from './view-all-ongoing-deliveries/view-all-ongoing-deliveries.component';
 import { ViewAllCompletedDeliveriesComponent } from './view-all-completed-deliveries/view-all-completed-deliveries.component';
 import { IndexComponent } from './index/index.component';
+import {
+  ViewDeliveryCompanyDetailsComponent
+} from './view-delivery-company-details/view-delivery-company-details.component';
 
 const routes: Routes = [
-  {path: '', redirectTo: '/index', pathMatch: 'full'},
-  {path: 'index', component: IndexComponent},
-  {path: 'viewAllOngoingDeliveries', component: ViewAllOngoingDeliveriesComponent},
-  {path: 'viewAllTransactions', component: ViewAllTransactionsComponent},
-  {path: 'viewAllCompletedDeliveries', component: ViewAllCompletedDeliveriesComponent}
+  { path: '', redirectTo: '/index', pathMatch: 'full' },
+  { path: 'index', component: IndexComponent },
+  { path: 'viewAllOngoingDeliveries', component: ViewAllOngoingDeliveriesComponent },
+  { path: 'viewAllTransactions', component: ViewAllTransactionsComponent },
+  { path: 'viewAllCompletedDeliveries', component: ViewAllCompletedDeliveriesComponent },
+  { path: 'profile', component: ViewDeliveryCompanyDetailsComponent }
 ];
 
 @NgModule({
